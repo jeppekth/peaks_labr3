7e2f539962e47e4c772d469f50b35513b491d5c3
Jeppe Thingholm <jeppe.thingholm@fys.uio.no>
2025-06-30 09:59:41 +0200

    nist isotopes mass

---
+++ NIST_isotope_mass.py
@@ -0,0 +1,40 @@
+class NIST_isotope_mass:
+    def __init__(self, filename):
+        self._isotope_dict = {}
+        with open(filename) as f:
+            at_data = False
+
+            symbol = ""
+
+            for l in f:
+                if l.isspace() or l == "":
+                    continue
+
+                if not at_data and "_" not in l:
+                    continue
+                elif not at_data and "_" in l:
+                    at_data = True
+                    continue
+                elif at_data and "_" in l:
+                    continue
+
+                if not l[0:3].isspace():
+                    symbol = l[4:6].replace(" ", "")
+                    self._isotope_dict[symbol] = {"Z": int(l[0:3]), "isotopes mass": {}}
+                
+                A = int(l[8:11])
+                mass_string_end = l.find("(")
+                mass = float(l[13:mass_string_end])
+
+                self._isotope_dict[symbol]["isotopes mass"][A] = mass
+    
+    def get_isotope_mass(self, symbol, A):
+        return self._isotope_dict[symbol]["isotopes mass"][A]
+    
+    def get_Z(self, symbol):
+        return self._isotope_dict[symbol]["Z"]
+
+
+
+
+
